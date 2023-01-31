dc9a66be325228b812e920a7492ea031ed4f55f4
RahulAgnihotri1998 <rahulagnihotri1998@gmail.com>
2023-01-31 21:21:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+
+function updateWordCount() {
+    var text = document.getElementById("evaluatedText").value;
+        var wordCount = text.trim() ? text.trim().split(" ").length : 0;
+        document.getElementById("wordCount").innerHTML = wordCount;
+}
